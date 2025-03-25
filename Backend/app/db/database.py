10a99fa579d41171ceb836cbe300dbb75f75cc97
MICHEE CEPHAS <123488406+skyson-ai@users.noreply.github.com>
2025-03-25 23:41:21 +0000

    modify backend

--- Backend/app/db/database.py
+++ Backend/app/db/database.py
@@ -3,10 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-DATABASE_URL = settings.DATABASE_URL
-if "psycopg2" in DATABASE_URL:
-    DATABASE_URL = DATABASE_URL.replace("postgresql+psycopg2://", "postgresql+psycopg://")
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = settings.DATABASE_URL.replace("postgresql+psycopg2://", "postgresql+psycopg://")
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
