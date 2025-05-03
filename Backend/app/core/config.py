5dedb2f059b9db1b5d2afe7132ab534c94c05738
MICHEE CEPHAS <123488406+skyson-ai@users.noreply.github.com>
2025-05-03 11:26:41 +0000

    Update Backend

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://blog_user:L270127N@localhost:5432/blog_db?client_encoding=utf8")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY", "huhfhufh66537687dhfjbchvkfogugkfhbh")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
--- Backend/app/db/database.py
+++ Backend/app/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-DATABASE_URL = settings.DATABASE_URL.replace("postgresql+psycopg2://", "postgresql+psycopg://")
+DATABASE_URL = settings.DATABASE_URL.replace("postgres://", "postgresql+psycopg://")
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
