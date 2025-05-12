32f41a2bc8bfe6bc8498a5400f37bdd6cc73ea1b
MICHEE CEPHAS <123488406+skyson-ai@users.noreply.github.com>
2025-05-12 22:31:12 +0000

    fixed auth issues

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -1,16 +1,17 @@
 from pydantic_settings import BaseSettings
-
 from pydantic import PostgresDsn, validator
 
 class Settings(BaseSettings):
-    DATABASE_URL: PostgresDsn  
+    DATABASE_URL: PostgresDsn
     SECRET_KEY: str
     FRONTEND_URL: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30  # Durée par défaut : 30 minutes
+    ALGORITHM: str = "HS256"  # Algorithme JWT
 
     @validator("DATABASE_URL", pre=True)
     def clean_database_url(cls, v):
         if isinstance(v, str) and v.startswith("DATABASE_URL="):
-            return v[len("DATABASE_URL="):]  
+            return v[len("DATABASE_URL="):]
         return v
 
     class Config:
